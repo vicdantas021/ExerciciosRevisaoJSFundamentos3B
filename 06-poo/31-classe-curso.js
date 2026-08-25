bedc81630e35e2d525ab06afc12430db8cf3f25a
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-25 16:48:24 -0300

    Adicionando informações ao arquivo 31-classe-curso.js

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,18 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso {
+    constructor(nome, duracao, modalidade){
+        this.nome = nome;
+        this.duracao = duracao;
+        this.modalidade = modalidade;
+    }
+    exibirInformacoes(){
+        console.log(`Nome: ${this.nome}, Duração: ${this.duracao}`);
+    }
+}
+
+const cursos = new Curso ("Desenvolvimento de Sistemas", "2 anos", "Presencial")
+cursos.exibirInformacoes()
+const cursos1 = new Curso ("Metalurgia", "2 anos", "Presencial")
+cursos1.exibirInformacoes()
