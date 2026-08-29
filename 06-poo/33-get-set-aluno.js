ff01639b565d8b746f5849607d5b320b4aac80c3
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-29 17:52:59 -0300

    Adicionando informações ao arquivo 33-get-set-alunos.js

--- 06-poo/33-get-set-aluno.js
+++ 06-poo/33-get-set-aluno.js
@@ -4,3 +4,22 @@
 // Crie um aluno, altere sua nota e mostre o resultado.
 //
 // Escreva sua solução abaixo:
+
+class Aluno {
+    #nota = 0;
+
+    get nota() {
+        return this.#nota;
+    } 
+    set nota(valor) {
+        if (valor >= 0 && valor <= 10) {
+            this.#nota = valor;
+        } else {
+            console.log("Nota inválida. A nota deve estar entre 0 e 10.");
+        }   
+    }
+}
+
+const aluno = new Aluno();
+aluno.nota = 8; 
+console.log(`Nota do aluno: ${aluno.nota}`); 
