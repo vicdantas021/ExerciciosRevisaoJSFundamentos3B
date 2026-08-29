1d96b34e525740cfba73bb7ef852768d8dcbc5be
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-29 18:13:00 -0300

    Adicionando informações ao arquivo 34-herenca-produto.js

--- 06-poo/34-heranca-produto.js
+++ 06-poo/34-heranca-produto.js
@@ -5,3 +5,29 @@
 //
 // Escreva sua solução abaixo:
 
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    exibirDados() {
+        return `Nome: ${this.nome}, Preço: R$${this.preco.toFixed(2)}`;
+    }
+}
+class ProdutoDigital extends Produto {
+    constructor(nome, preco, tamanhoArquivo) {
+        super(nome, preco);
+        this.tamanhoArquivo = tamanhoArquivo;
+    }
+    exibirDados() {
+        return `${super.exibirDados()}, Tamanho do Arquivo: ${this.tamanhoArquivo} MB`;
+    }
+}
+
+console.log("Produto Físico:");
+const produtoFisico = new Produto("Livro", 29.90);
+console.log(produtoFisico.exibirDados());
+
+console.log("Produto Digital:");
+const produtoDigital = new ProdutoDigital("Jogo", 199.90, 500);
+console.log(produtoDigital.exibirDados());
